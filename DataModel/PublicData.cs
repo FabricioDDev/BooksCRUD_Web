e87c9a53349ee4123a48de5b7fd46f949a9ab4fb
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2022-10-28 11:53:53 +0200

    PúblicData-ListFunction-Created

--- DataModel/PublicData.cs
+++ DataModel/PublicData.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using DB;
 
 namespace DataModel
 {
-    class PublicData
+    public class PublicData
     {
+        public PublicData()
+        {
+            data = new DataAccess();
+        }
+        private DataAccess data;
+        public List<Public> Listing()
+        {
+            List<Public> List = new List<Public>();
+            try
+            {
+                data.Query("select Id, Name from PublicTable");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    Public aux = new Public();
+                    aux.Id = (int)data.PropReader["Id"];
+                    aux.Name = (string)data.PropReader["Name"];
+                    List.Add(aux);
+                }
+                return List;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { data.Close(); }
+        }
     }
 }
