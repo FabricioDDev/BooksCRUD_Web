984368ab540c5914f51ff4f1d9e7e529e0560e89
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2022-10-28 11:57:34 +0200

    CategoryData-ListFunction-Created

--- DataModel/CategoryData.cs
+++ DataModel/CategoryData.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DomainModel;
+using DB;
 
 namespace DataModel
 {
-    class CategoryData
+    public class CategoryData
     {
+        public CategoryData()
+        {
+            data = new DataAccess();
+        }
+        private DataAccess data;
+        public List<Category> Listing()
+        {
+            List<Category> List = new List<Category>();
+            try
+            {
+                data.Query("select Id, Name from CategoryTable");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    Category aux = new Category();
+                    aux.Id = (int)data.PropReader["Id"];
+                    aux.Name = (string)data.PropReader["Name"];
+                    List.Add(aux);
+                }
+                return List;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally { data.Close(); }
+        }
     }
 }
