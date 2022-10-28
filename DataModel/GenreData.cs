5908463be6781c149501cd552e332f448e2c4f6d
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2022-10-28 11:51:00 +0200

    GenreData-ListFunction-Created

--- DataModel/GenreData.cs
+++ DataModel/GenreData.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DB;
+using DomainModel;
 
 namespace DataModel
 {
-    class GenreData
+    public class GenreData
     {
+        public GenreData()
+        {
+            data = new DataAccess();
+        }
+        private DataAccess data;
+
+        public List<Genre> Listing()
+        {
+            List<Genre> List = new List<Genre>();
+            try
+            {
+                data.Query("select Id, Name from CategoryTable");
+                data.Read();
+                while (data.PropReader.Read())
+                {
+                    Genre aux = new Genre();
+                    aux.Id = (int)data.PropReader["Id"];
+                    aux.Name = (string)data.PropReader["Name"];
+                    List.Add(aux);
+                }
+                return List;
+            }
+            catch (Exception ex) {
+                throw ex;
+            }
+            finally { data.Close(); }
     }
 }
