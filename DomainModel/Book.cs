8470916b70e0e6a7a6a0403d0057d9ed2762e2cc
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2022-10-28 11:25:33 +0200

    AtributesDomainModel

--- DomainModel/Book.cs
+++ DomainModel/Book.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace DomainModel
 {
-    class Book
+    public class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Url { get; set; }
+        public string Cover { get; set; }
+        public Category Category { get; set; }
+        public Genre Genre { get; set; }
+        public Public Public { get; set; }
     }
 }
--- DomainModel/Public.cs
+++ DomainModel/Public.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace DomainModel
 {
-    class Public
+    public class Public
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
