8a5a82dcea575e2ac199484d005a1a38c0306480
Fabricio DiPaola <fabriciodamiandipaola@gmail.com>
2022-10-29 11:18:53 +0200

    MainView-Controlls-Created

---
+++ BooksCRUD/Views/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BooksCRUD.Views.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <!-- Filtro Buscador(x Nombre, Autor) -->
+            <asp:TextBox ID="TxtSearch" runat="server"></asp:TextBox>
+            <asp:Button ID="BtnSearch" runat="server" Text="Button" />
+            <!-- Filtro DropDown(x Categoria) -->
+            <asp:DropDownList ID="DdlCategory" runat="server"></asp:DropDownList>
+            <!-- Filtro DropDown(x Genero) -->
+            <asp:DropDownList ID="DdlGenre" runat="server"></asp:DropDownList>
+            <!-- Filtro DropDown(x Publico) -->
+            <asp:DropDownList ID="DdlPublic" runat="server"></asp:DropDownList>
+            <!-- Filtro DropDown(x Año) -->
+            <asp:DropDownList ID="DdlYear" runat="server"></asp:DropDownList>
+            <!-- btn ADD -->
+            <asp:Button ID="BtnAddBook" runat="server" Text="Button" />
+            <!-- GridView(con btn delete, uodate) -->
+            <asp:GridView ID="GvBook" runat="server"></asp:GridView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ BooksCRUD/Views/Default.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace BooksCRUD.Views
+{
+
+
+    public partial class Default
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control TxtSearch.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtSearch;
+
+        /// <summary>
+        /// Control BtnSearch.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnSearch;
+
+        /// <summary>
+        /// Control DdlCategory.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlCategory;
+
+        /// <summary>
+        /// Control DdlGenre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlGenre;
+
+        /// <summary>
+        /// Control DdlPublic.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlPublic;
+
+        /// <summary>
+        /// Control DdlYear.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DdlYear;
+
+        /// <summary>
+        /// Control BtnAddBook.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BtnAddBook;
+
+        /// <summary>
+        /// Control GvBook.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GvBook;
+    }
+}
